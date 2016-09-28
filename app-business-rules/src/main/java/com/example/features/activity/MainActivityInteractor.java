229673407f86772b92002f29ca9a6f04126cec15
Ander Turrez <ander@harbourfront.se>
2016-09-28 11:42:39 +0200

    Create a MainActivity UseCase for not working directly with Repository from presenter, which is incorrect

---
+++ app-business-rules/src/main/java/com/example/features/activity/MainActivityInteractor.java
@@ -0,0 +1,8 @@
+package com.example.features.activity;
+
+/**
+ * Created by Ander Túrrez on 28/09/16.
+ */
+
+public class MainActivityInteractor {
+}
---
+++ presentation/src/main/java/com/test/ander/testingcleanarchitecturewithdaggerandrxjava/features/getuser/userinfo/UpdateCurrentUserInfoEvent.java
@@ -0,0 +1,8 @@
+package com.test.ander.testingcleanarchitecturewithdaggerandrxjava.features.getuser.userinfo;
+
+/**
+ * Created by Ander Túrrez on 28/09/16.
+ */
+
+public class UpdateCurrentUserInfoEvent {
+}
