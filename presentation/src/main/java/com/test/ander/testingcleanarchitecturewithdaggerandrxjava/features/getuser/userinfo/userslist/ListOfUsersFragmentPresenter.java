9720ac05a5bcb1e71efa3164a8cdf684675ea13c
Ander Turrez <ander@harbourfront.se>
2016-09-30 09:26:48 +0200

    Create the Users List Presenter (the class that implements the MVP Presenter)

--- presentation/src/main/java/com/test/ander/testingcleanarchitecturewithdaggerandrxjava/features/getuser/userinfo/userslist/ListOfUsersFragmentPresenter.java
+++ presentation/src/main/java/com/test/ander/testingcleanarchitecturewithdaggerandrxjava/features/getuser/userinfo/userslist/ListOfUsersFragmentPresenter.java
@@ -1,8 +1,26 @@
 package com.test.ander.testingcleanarchitecturewithdaggerandrxjava.features.getuser.userinfo.userslist;
 
+import com.example.customscopes.PerActivity;
+import com.example.features.getuser.userinfo.UserInfoUseCase;
+
+import javax.inject.Inject;
+
 /**
  * Created by Ander Túrrez on 29/09/16.
  */
+@PerActivity
+public class ListOfUsersFragmentPresenter implements MVPUsersList.Presenter {
 
-public class ListOfUsersFragmentPresenter {
+    private MVPUsersList.View view;
+    private UserInfoUseCase interactor;
+
+    @Inject
+    public ListOfUsersFragmentPresenter(UserInfoUseCase interactor) {
+        this.interactor = interactor;
+    }
+
+    @Override
+    public void setView(MVPUsersList.View view) {
+        this.view = view;
+    }
 }
