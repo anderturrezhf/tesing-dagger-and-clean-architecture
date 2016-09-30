3d09e5e69af2c28a10500127fe86dfdb012dd28c
Ander Turrez <ander@harbourfront.se>
2016-09-30 09:23:56 +0200

    Create new elements related to the User List Fragment (Presenter, Interactor, Adapter for the RecyclerView...)

---
+++ presentation/src/main/java/com/test/ander/testingcleanarchitecturewithdaggerandrxjava/features/getuser/userinfo/userslist/UsersListAdapter.java
@@ -0,0 +1,8 @@
+package com.test.ander.testingcleanarchitecturewithdaggerandrxjava.features.getuser.userinfo.userslist;
+
+/**
+ * Created by Ander Túrrez on 29/09/16.
+ */
+
+public class UsersListAdapter {
+}
